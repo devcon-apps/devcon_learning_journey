7387404743aebc3b57d5adc9bd7df1150c30a707
KR030152 <krishna.r@cerner.com>
2017-11-03 22:44:30 +0530

    added react learner page

--- app/javascript/packs/learner/app.jsx
+++ app/javascript/packs/learner/app.jsx
@@ -4,23 +4,21 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
 
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
-
-Hello.defaultProps = {
-  name: 'David'
+export default class App extends React.Component {
+    render() {
+        return (
+            <div>
+                Welcome to Learner Page
+            </div>
+        )
+    }
 }
 
-Hello.propTypes = {
-  name: PropTypes.string
-}
-
+window.React = React
 document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <Hello name="React" />,
-    document.body.appendChild(document.createElement('div')),
-  )
+    ReactDOM.render(
+        <App />,
+        document.body.appendChild(document.createElement('div')),
+    )
 })
